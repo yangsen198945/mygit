090076e98ea11b7254c87673674ef93dd7608110
yangsen198945 <570559258@qq.com>
2018-04-04 22:47:46 +0800

    删除所有注释

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ public class Main {
     // 新增注释
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        // 这边修改
         System.out.println("哈哈");
     }
 }
